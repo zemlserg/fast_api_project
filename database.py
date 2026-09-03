df7bcd07d09991268902af0182a34083cb824fe0
zemlserg <uralrus1@yandex.ru>
2026-09-03 23:00:59 +0500

    fix: change wrong connection string format

--- database.py
+++ database.py
@@ -4,8 +4,7 @@ from typing import Annotated
 from fastapi import Depends
 from sqlalchemy.ext.asyncio import AsyncSession
 # 1. Настройка URL для SQLite (асинхронный драйвер aiosqlite)
-DATABASE_URL = "postgresql+asyncpg://postgres.dnjydqwshqllzlqfdjhn:steelzsv0826@://supabase.com"
-
+DATABASE_URL = "postgresql://postgres:steelzsv0826@db.dnjydqwshqllzlqfdjhn.supabase.co:5432/postgres"
 # 2. Создание движка (Engine)
 engine = create_async_engine(DATABASE_URL, echo=True)
 async def get_db():
